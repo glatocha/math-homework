16b2f57ea514baceae030272b9c1f745907848f7
glatocha <glatocha@gmail.com>
2023-08-21 22:40:06 +0800

    changed to table

--- script.js
+++ script.js
@@ -21,29 +21,46 @@ showBtnEl.addEventListener("click", () => {
 function generateNew() {
   listEl.innerHTML = "";
   for (i = 1; i <= noOfTasks; i++) {
+    //preparing the table ROW HTML Structure
+    const tableRowEl = document.createElement("tr");
+    const tableNoEl = document.createElement("td");
+    tableNoEl.innerText = `${i}.`;
+    const tableFirstNumEl = document.createElement("td");
+    const tableOperatorEl = document.createElement("td");
+    const tableSecondNumEl = document.createElement("td");
+    const tableEqualEl = document.createElement("td");
+    tableEqualEl.innerText = "=";
+    const tableResultEl = document.createElement("td");
+    tableRowEl.appendChild(tableNoEl);
+    tableNoEl.classList.add("text-left");
+    tableRowEl.appendChild(tableFirstNumEl);
+    tableRowEl.appendChild(tableOperatorEl);
+    tableRowEl.appendChild(tableSecondNumEl);
+    tableRowEl.appendChild(tableEqualEl);
+    tableRowEl.appendChild(tableResultEl);
+    tableResultEl.classList.add("result");
+    tableResultEl.classList.add("hide");
+
+    //calculations start
     var result = 0;
     const numberOne = Math.floor(Math.random() * (max - 1)) + 1;
     const opAdding = Math.random() < 0.5; //True for -, False for +
-    var outputString = "";
-    console.log("numberOne :>> ", numberOne);
+    tableFirstNumEl.innerText = numberOne;
 
     if (opAdding) {
       const numberTwo = Math.floor(Math.random() * (max - numberOne)) + 1;
       result = numberOne + numberTwo;
-      outputString = `${numberOne} + ${numberTwo} = `;
+      tableOperatorEl.innerText = "+";
+      tableSecondNumEl.innerText = numberTwo;
+      tableResultEl.innerText = result;
     } else {
       //subtracting
       const numberTwo = Math.floor(Math.random() * (numberOne - 1)) + 1;
       result = numberOne - numberTwo;
-      outputString = `${numberOne} - ${numberTwo} = `;
+      tableOperatorEl.innerText = "-";
+      tableSecondNumEl.innerText = numberTwo;
+      tableResultEl.innerText = result;
     }
-    const listItemEl = document.createElement("li");
-    listItemEl.innerText = outputString;
-    const resEl = document.createElement("span");
-    resEl.classList.add("result");
-    resEl.classList.add("hide");
-    resEl.innerText = result;
-    listItemEl.appendChild(resEl);
-    listEl.appendChild(listItemEl);
+    listEl.appendChild(tableRowEl);
   }
 }
