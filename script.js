a70594b34b1ff4ac016bd723e73f1b3df7047e20
glatocha <glatocha@gmail.com>
2024-02-11 14:08:30 +0100

    finished three options of exercise

--- script.js
+++ script.js
@@ -4,73 +4,29 @@
 const negative = false;
 // const max = 100;
 const listEl = document.getElementById("list");
-const genBtnEl = document.getElementById("genBtn");
+const gen1BtnEl = document.getElementById("gen1Btn");
+const gen2BtnEl = document.getElementById("gen2Btn");
+const gen3BtnEl = document.getElementById("gen3Btn");
 const showBtnEl = document.getElementById("showBtn");
 const multiplyEl = document.getElementById("multiply");
 const divideEl = document.getElementById("divide");
 
-// const dig_0 = document.createElement("img");
-// const dig_1 = document.createElement("img");
-// const dig_2 = document.createElement("img");
-// const dig_3 = document.createElement("img");
-// const dig_4 = document.createElement("img");
-// const dig_5 = document.createElement("img");
-// const dig_6 = document.createElement("img");
-// const dig_7 = document.createElement("img");
-// const dig_8 = document.createElement("img");
-// const dig_9 = document.createElement("img");
-
-// const dig_P = document.createElement("img");
-// const dig_M = document.createElement("img");
-// const dig_X = document.createElement("img");
-// const dig_D = document.createElement("img");
-// const dig_E = document.createElement("img");
-
-// // dig_0.src = "assets/dig_0.png";
-// dig_1.src = "assets/dig_1.png";
-// dig_2.src = "assets/dig_2.png";
-// dig_3.src = "assets/dig_3.png";
-// dig_4.src = "assets/dig_4.png";
-// dig_5.src = "assets/dig_5.png";
-// dig_6.src = "assets/dig_6.png";
-// dig_7.src = "assets/dig_7.png";
-// dig_8.src = "assets/dig_8.png";
-// dig_9.src = "assets/dig_9.png";
-// dig_P.src = "assets/dig_P.png";
-// dig_M.src = "assets/dig_M.png";
-// dig_X.src = "assets/dig_X.png";
-// dig_D.src = "assets/dig_D.png";
-// dig_E.src = "assets/dig_E.png";
-
-// dig_0.classList.add("number-ing");
-// dig_1.classList.add("number-ing");
-// dig_2.classList.add("number-ing");
-// dig_3.classList.add("number-ing");
-// dig_4.classList.add("number-ing");
-// dig_5.classList.add("number-ing");
-// dig_6.classList.add("number-ing");
-// dig_7.classList.add("number-ing");
-// dig_8.classList.add("number-ing");
-// dig_9.classList.add("number-ing");
-// dig_P.classList.add("operand-ing");
-// dig_M.classList.add("operand-ing");
-// dig_X.classList.add("operand-ing");
-// dig_D.classList.add("operand-ing");
-// dig_E.classList.add("operand-ing");
-
-// fetch("assets/dig_0.png")
-//   .then((res) => res.blob())
-//   .then((blob) => URL.createObjectURL(blob))
-//   .then((dataUrl) => {
-//     dig_0.src = dataUrl;
-//   });
-
 var showResults = false;
 
-window.addEventListener("DOMContentLoaded", generateNew);
-genBtnEl.addEventListener("click", (e) => {
+//window.addEventListener("DOMContentLoaded", () => {
+//   generateNew();
+// });
+gen1BtnEl.addEventListener("click", (e) => {
   e.preventDefault();
-  generateNew();
+  generateNew1();
+});
+gen2BtnEl.addEventListener("click", (e) => {
+  e.preventDefault();
+  generateNew2();
+});
+gen3BtnEl.addEventListener("click", (e) => {
+  e.preventDefault();
+  generateNew3();
 });
 
 showBtnEl.addEventListener("click", (e) => {
@@ -88,7 +44,7 @@ showBtnEl.addEventListener("click", (e) => {
   }
 });
 
-function generateNew() {
+function generateNew1() {
   const noOfTasks = document.getElementById("noOfTasks").value;
   const max = document.getElementById("maxValue").value;
   listEl.innerHTML = "";
@@ -110,7 +66,85 @@ function generateNew() {
       result = numberOne - numberTwo;
       firstOperand = "-";
     }
-    listEl.appendChild(makeTableRow(i, numberOne, firstOperand, numberTwo, 0, 0, result));
+    listEl.appendChild(makeTableRow(i, numberOne, firstOperand, numberTwo, null, null, result));
+  }
+}
+
+function generateNew2() {
+  const noOfTasks = document.getElementById("noOfTasks").value;
+  const max = document.getElementById("maxValue").value;
+  listEl.innerHTML = "";
+  showBtnEl.innerText = "Show results";
+  for (i = 1; i <= noOfTasks; i++) {
+    //calculations start
+    var result = 0;
+    const numberOne = Math.floor(Math.random() * (max - 1)) + 1;
+    let numberTwo = 0;
+    const opAdding = Math.random() < 0.5; //True for -, False for +
+
+    if (opAdding) {
+      numberTwo = Math.floor(Math.random() * (max - numberOne)) + 1;
+      result = numberOne + numberTwo;
+      firstOperand = "+";
+    } else {
+      //subtracting
+      numberTwo = Math.floor(Math.random() * (numberOne - 1)) + 1;
+      result = numberOne - numberTwo;
+      firstOperand = "-";
+    }
+
+    //third number
+    const opAdding2 = Math.random() < 0.5; //True for -, False for +
+    let numberThree = 0;
+
+    if (opAdding2) {
+      numberThree = Math.floor(Math.random() * (max - result)) + 1;
+      result = result + numberThree;
+      seconfOperand = "+";
+    } else {
+      //subtracting
+      numberThree = Math.floor(Math.random() * (result - 1)) + 1;
+      result = result - numberThree;
+      seconfOperand = "-";
+    }
+
+    listEl.appendChild(
+      makeTableRow(i, numberOne, firstOperand, numberTwo, seconfOperand, numberThree, result)
+    );
+  }
+}
+
+function generateNew3() {
+  const noOfTasks = document.getElementById("noOfTasks").value;
+  const max = document.getElementById("maxValue").value;
+  listEl.innerHTML = "";
+  showBtnEl.innerText = "Show results";
+  for (i = 1; i <= noOfTasks; i++) {
+    //calculations start
+    var result = 0;
+    const numberOne = Math.floor(Math.random() * 10) + 1;
+    const numberTwo = Math.floor(Math.random() * 10) + 1;
+    firstOperand = "*";
+    result = numberOne * numberTwo;
+
+    //third number
+    const opAdding2 = Math.random() < 0.5; //True for -, False for +
+    let numberThree = 0;
+
+    if (opAdding2) {
+      numberThree = Math.floor(Math.random() * (max - result)) + 1;
+      result = result + numberThree;
+      seconfOperand = "+";
+    } else {
+      //subtracting
+      numberThree = Math.floor(Math.random() * (result - 1)) + 1;
+      result = result - numberThree;
+      seconfOperand = "-";
+    }
+
+    listEl.appendChild(
+      makeTableRow(i, numberOne, firstOperand, numberTwo, seconfOperand, numberThree, result)
+    );
   }
 }
 
@@ -125,6 +159,9 @@ function makeTableRow(number, firstNumber, firstOp, secondNumber, secondOp, thir
   const tableSecondNumEl = document.createElement("td");
   const tableEqualEl = document.createElement("td");
 
+  const tableOperator2El = document.createElement("td");
+  const tableThirdNumEl = document.createElement("td");
+
   // tableEqualEl.innerText = "=";
   tableEqualEl.append(operandToPic("="));
 
@@ -134,6 +171,10 @@ function makeTableRow(number, firstNumber, firstOp, secondNumber, secondOp, thir
   tableRowEl.appendChild(tableFirstNumEl);
   tableRowEl.appendChild(tableOperatorEl);
   tableRowEl.appendChild(tableSecondNumEl);
+  if (secondOp) {
+    tableRowEl.appendChild(tableOperator2El);
+    tableRowEl.appendChild(tableThirdNumEl);
+  }
   tableRowEl.appendChild(tableEqualEl);
   tableRowEl.appendChild(tableResultEl);
   tableResultEl.classList.add("result");
@@ -148,6 +189,11 @@ function makeTableRow(number, firstNumber, firstOp, secondNumber, secondOp, thir
   tableSecondNumEl.append(numToPic(secondNumber));
   tableOperatorEl.append(operandToPic(firstOp));
   tableResultEl.append(numToPic(result));
+
+  if (secondOp) {
+    tableOperator2El.append(operandToPic(secondOp));
+    tableThirdNumEl.append(numToPic(thirdNumber));
+  }
 
   return tableRowEl;
 }
@@ -176,34 +222,6 @@ function numToPic(value) {
   return element;
 }
 
-function createImg(value, cssClass) {
-  const img = document.createElement("img");
-  img.classList.add(cssClass);
-  img.src = `assets/${getShort(value)}`;
-
-  return img;
-}
-
-function getShort(value) {
-  const table = {
-    0: "dig_0.png",
-    1: "dig_1.png",
-    2: "dig_2.png",
-    3: "dig_3.png",
-    4: "dig_4.png",
-    5: "dig_5.png",
-    6: "dig_6.png",
-    7: "dig_7.png",
-    8: "dig_8.png",
-    9: "dig_9.png",
-    "+": "dig_P.png",
-    "-": "dig_M.png",
-    "*": "dig_X.png",
-    "/": "dig_D.png",
-    "=": "dig_E.png",
-  };
-  return table[value];
-}
 function getShortObj(value) {
   const table = {
     0: dig_0,
@@ -241,3 +259,32 @@ function getShortObj(value) {
 //     output.appendChild(img);
 //   }
 // });
+
+// function createImg(value, cssClass) {
+//   const img = document.createElement("img");
+//   img.classList.add(cssClass);
+//   img.src = `assets/${getShort(value)}`;
+
+//   return img;
+// }
+
+// function getShort(value) {
+//   const table = {
+//     0: "dig_0.png",
+//     1: "dig_1.png",
+//     2: "dig_2.png",
+//     3: "dig_3.png",
+//     4: "dig_4.png",
+//     5: "dig_5.png",
+//     6: "dig_6.png",
+//     7: "dig_7.png",
+//     8: "dig_8.png",
+//     9: "dig_9.png",
+//     "+": "dig_P.png",
+//     "-": "dig_M.png",
+//     "*": "dig_X.png",
+//     "/": "dig_D.png",
+//     "=": "dig_E.png",
+//   };
+//   return table[value];
+// }
